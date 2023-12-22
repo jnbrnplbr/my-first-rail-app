bf6528c4618a95ce8aabc1642d2e144a0661b059
brye <jnbrnplbr@gmail.com>
2023-12-22 19:55:19 +0800

    add: new migrations for stocks table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_12_20_113858) do
+ActiveRecord::Schema[7.1].define(version: 2023_12_22_115150) do
+  create_table "stocks", force: :cascade do |t|
+    t.string "ticker"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_stocks_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
