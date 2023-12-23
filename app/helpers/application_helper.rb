67c1a0a33ac89a87d25d0bf5592ba69cd20026ef
brye <jnbrnplbr@gmail.com>
2023-12-23 18:12:42 +0800

    upd: login page and add logo

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
     def page_without_navbar 
-        routes = ['/users/sign_in', '/users/sign_up']
+        routes = ['/users/sign_in', '/users/sign_up','/users/password/new']
         return !(routes.include?request.path)
     end
 end
