e13a03750f691a42a51de71a7572f43ef82d0e7f
brye <jnbrnplbr@gmail.com>
2023-12-26 00:23:56 +0800

    add: admin panel and redesign

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < ActionController::Base
+  layout :set_layout
   protect_from_forgery with: :exception
+
+  private
+
+  def set_layout
+    if user_signed_in?
+      "admin"
+    else  
+      "application"
+    end
+  end
 end
